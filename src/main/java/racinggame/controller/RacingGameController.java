ceadb1559a465ae5233f14d1f5275b11303b85de
kimtaejun97 <kim_taejun@naver.com>
2022-01-26 18:25:56 +0900

    feat: RAcingGameController 생성.
    - GameView, InputView를 가진다.

--- src/main/java/racinggame/controller/RacingGameController.java
+++ src/main/java/racinggame/controller/RacingGameController.java
@@ -1,2 +1,16 @@
-package racinggame.controller;public class RacingGameController {
+package racinggame.controller;
+
+import racinggame.view.GameView;
+import racinggame.view.InputView;
+
+public class RacingGameController {
+
+    private final InputView inputView;
+    private final GameView gameView;
+
+    public RacingGameController(InputView inputView, GameView gameView) {
+        this.inputView = inputView;
+        this.gameView = gameView;
+    }
+
 }
