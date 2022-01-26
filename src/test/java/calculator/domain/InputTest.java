4b9d06b5eefebb4db50fab6ef61a2a91459f8a70
kimtaejun97 <kim_taejun@naver.com>
2022-01-26 18:28:20 +0900

    fix(rollback): 파일 삭제 복구..

--- src/test/java/calculator/domain/InputTest.java
+++ src/test/java/calculator/domain/InputTest.java
@@ -1,2 +1,38 @@
-package calculator.domain;public class InputTest {
+package calculator.domain;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.ginsberg.junit.exit.ExpectSystemExit;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class InputTest {
+
+    @ValueSource(strings = {"", " ", "    "})
+    @ParameterizedTest
+    void checkIsBlank_입력으로_공백이_들어옴(String blank) {
+        // then
+        assertThrows(IllegalArgumentException.class, () -> Validator.checkIsBlack(blank));
+    }
+
+    @Test
+    void checkIsBlank_공백이_아님() {
+        // then
+        assertDoesNotThrow(() -> Validator.checkIsBlack("1 + 2"));
+    }
+
+    @ExpectSystemExit
+    @Test
+    void q를_입력하면_종료된다() {
+        // given
+        InputStream inputStream = new ByteArrayInputStream("q".getBytes());
+        System.setIn(inputStream);
+
+        // then
+        Input.getUserInput();
+    }
 }
