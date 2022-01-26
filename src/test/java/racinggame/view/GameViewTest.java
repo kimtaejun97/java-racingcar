c2fa6a6c7c51cb288d80bc9b4e9428129c307949
kimtaejun97 <kim_taejun@naver.com>
2022-01-26 23:09:06 +0900

    test: racingCars, gameView 테스트전 미리 생성, 생성자 테스트 assertThat으로 변경

--- src/test/java/racinggame/view/GameViewTest.java
+++ src/test/java/racinggame/view/GameViewTest.java
@@ -1,40 +1,39 @@
 package racinggame.view;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import racinggame.domain.RacingCar;
 
 class GameViewTest {
 
+    List<RacingCar> racingCars = new ArrayList<>(
+            Arrays.asList(new RacingCar("car1", 1), new RacingCar("car2", 3)));
+
+    GameView gameView = new GameView(racingCars);
+
     @Test
     void 상태를_출력할_자동차_리스트를_받아_생성한다() throws NoSuchFieldException, IllegalAccessException {
 
-        // given
-        String[] cars = {"car1", "car2"};
-        List<RacingCar> racingCars = RacingCar.racingCarOf(cars);
-
         // when
-        GameView gameView = new GameView(racingCars);
         Field racingCarViewsField = GameView.class.getDeclaredField("racingCarViews");
         racingCarViewsField.setAccessible(true);
 
         List<RacingCarView> racingCarViews = (List<RacingCarView>) racingCarViewsField.get(gameView);
         // then
-        System.out.println(racingCarViews);
+
+        assertThat(racingCarViews.size()).isEqualTo(2);
     }
 
     @Test
     void 현재_자동차들의_상태를_출력한다() {
-
-        // given
-        String[] cars = {"car1", "car2"};
-        List<RacingCar> racingCars = RacingCar.racingCarOf(cars);
-        GameView gameView = new GameView(racingCars);
 
         // when
         ByteArrayOutputStream out = new ByteArrayOutputStream();
@@ -43,7 +42,7 @@ class GameViewTest {
         gameView.printProgress();
 
         // then
-        assertTrue(out.toString().contains("car1"));
-        assertTrue(out.toString().contains("car2"));
+        assertTrue(out.toString().contains("car1 : -"));
+        assertTrue(out.toString().contains("car2 : ---"));
     }
 }
