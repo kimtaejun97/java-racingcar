43956d2ce68add914897a04dc2f538eca5bd81f5
kimtaejun97 <kim_taejun@naver.com>
2022-01-26 21:00:17 +0900

    feat: RacingCar 클래스에서 RacingCarView 의존 제거.
    - mvc 패턴에 기반하기 위해 의존 제거.
    - drawCurrPosition() 제거.

--- src/main/java/racinggame/domain/RacingCar.java
+++ src/main/java/racinggame/domain/RacingCar.java
@@ -3,15 +3,12 @@ package racinggame.domain;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import racinggame.view.InputView;
-import racinggame.view.RacingCarView;
 
 public class RacingCar {
 
     private static final int MINIMUM_FORWARD_VALUE = 4;
     private static final int INIT_POSITION = 0;
 
-    private final RacingCarView racingCarView;
     private String carName;
     private int carPosition;
 
@@ -21,8 +18,6 @@ public class RacingCar {
 
     public RacingCar(String carName, int position) {
         Validator.validateNameLength(carName);
-
-        racingCarView = new RacingCarView(this);
         this.carName = carName;
         this.carPosition = position;
     }
@@ -49,10 +44,6 @@ public class RacingCar {
 
     public void forward() {
         this.carPosition += 1;
-    }
-
-    public String drawCurrPosition() {
-        return this.racingCarView.drawCurrPosition();
     }
 
     public int getPosition() {
