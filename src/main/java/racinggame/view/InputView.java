ffc3957871c8afe21d8464016337abc62ac639b4
kimtaejun97 <kim_taejun@naver.com>
2022-01-26 21:10:02 +0900

    feat: 게임 종료 후 종료할지 물어보는 inputIsRestart() 기능 추가.

--- src/main/java/racinggame/view/InputView.java
+++ src/main/java/racinggame/view/InputView.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class InputView {
 
-    public InputView() {}
+    public InputView() {
+    }
 
     public String inputCarNames() {
         System.out.println("경주할 자동차 이름을 ','로 구분해서 입력하세요.(2대 이상)");
@@ -13,6 +14,11 @@ public class InputView {
 
     public String inputTrial() {
         System.out.print("시도 횟수를 입력해 주세요: ");
+        return getInput();
+    }
+
+    public String inputIsRestart() {
+        System.out.println("게임이 종료되었습니다. (종료 = q, 다시 시작 = 아무 키 입력)");
         return getInput();
     }
 
