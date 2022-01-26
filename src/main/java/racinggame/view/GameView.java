30110ec0a9ac77c16cb1eded3003084246bd94df
kimtaejun97 <kim_taejun@naver.com>
2022-01-26 21:05:53 +0900

    feat: GameView는 racingCarView 리스트를 가지도록 변경.
    기존: RacingCar 리스트를 가지고 있던 것에서 racingCar 리스트를 받아 RacingCarView 리스트를 생성하여
    가지도록 변경.

--- src/main/java/racinggame/view/GameView.java
+++ src/main/java/racinggame/view/GameView.java
@@ -2,34 +2,32 @@ package racinggame.view;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import racinggame.domain.Judge;
 import racinggame.domain.RacingCar;
 
 public class GameView {
 
     private static final String DELIMITER = ", ";
+    private static final String QUIT = "q";
 
-    private List<RacingCar> racingCars;
+    private List<RacingCarView> racingCarViews;
 
     public GameView(List<RacingCar> racingCars) {
-        this.racingCars = racingCars;
-    }
-
-    public List<RacingCar> getRacingCars() {
-        return racingCars;
+        racingCarViews = RacingCarView.racingCarOf(racingCars);
     }
 
     public void printProgress() {
         StringBuilder sb = new StringBuilder();
-        racingCars.forEach(car -> sb.append(car.drawCurrPosition()));
+        racingCarViews.forEach(carView -> sb.append(carView.drawCurrPosition()));
 
         System.out.println(sb);
     }
 
-    public void printWinners() {
-        List<String> winners = Judge.getWinners(racingCars);
-
+    public void printWinners(List<String> winners) {
         String result = winners.stream().collect(Collectors.joining(DELIMITER));
         System.out.println("최종 우승자: " + result);
+    }
+
+    public static boolean isQuit(String command) {
+        return command.equals(QUIT);
     }
 }
