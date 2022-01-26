efb2975837ff17b37dd1e0f2211021872312e330
kimtaejun97 <kim_taejun@naver.com>
2022-01-26 22:40:06 +0900

    test: controller getRacingCars 통합 테스트

---
+++ src/test/java/racinggame/controller/RacingGameControllerTest.java
@@ -0,0 +1,29 @@
+package racinggame.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import racinggame.domain.RacingCar;
+import racinggame.service.InputService;
+import racinggame.view.InputView;
+
+class RacingGameControllerTest {
+
+    RacingGameController controller = new RacingGameController(new InputView(), new InputService());
+
+    @Test
+    void getRacingCars() {
+        String userInput = "car1, car2";
+        InputStream inputStream = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(inputStream);
+
+        List<RacingCar> racingCars = controller.getRacingCars();
+
+        assertThat(racingCars.size()).isEqualTo(2);
+        assertThat(racingCars.get(0).getCarName()).isEqualTo("car1");
+        assertThat(racingCars.get(1).getCarName()).isEqualTo("car2");
+    }
+}
